3720c4d270981f7a64e7d82011f47cddd5d92f67
Otto Kekäläinen <otto@kekalainen.net>
2020-12-26 22:59:09 +0200

    WIP: Independent mariadb-plugin-columnstore packaging
    Build only the mariadb-plugin-columnstore plugin, so that it can be
    published separately as a self-standing release. Installation naturally
    requires installing MariaDB Server (or whatever dependencies the plugin
    has) but this way ColumnStore can have
    - it's own version number/scheme
    - it's own releases and release schedules
    - it's own customized build that is precise and fast (important for CI)
    - it's own clearly defined build dependencies that are easy to verify

--- debian/autobake-deb.sh
+++ debian/autobake-deb.sh
@@ -17,61 +17,6 @@ set -e
 # building the deb packages here.
 export DEB_BUILD_OPTIONS="nocheck $DEB_BUILD_OPTIONS"
 
-# Take the files and part of control from MCS directory
-if [[ -d storage/columnstore/columnstore/debian ]]; then
-  cp -v storage/columnstore/columnstore/debian/mariadb-plugin-columnstore.* debian/
-  echo >> debian/control
-  cat storage/columnstore/columnstore/debian/control >> debian/control
-fi
-
-# General CI optimizations to keep build output smaller
-if [[ $TRAVIS ]] || [[ $GITLAB_CI ]]
-then
-  # On both Travis and Gitlab the output log must stay under 4MB so make the
-  # build less verbose
-  sed '/Add support for verbose builds/,/^$/d' -i debian/rules
-
-  # MCOL-4149: ColumnStore builds are so slow and big that they must be skipped on
-  # both Travis-CI and Gitlab-CI
-  sed 's|-DPLUGIN_COLUMNSTORE=YES|-DPLUGIN_COLUMNSTORE=NO|' -i debian/rules
-  sed "/Package: mariadb-plugin-columnstore/,/^$/d" -i debian/control
-fi
-
-# Don't build or try to put files in a package for selected plugins and compontents on Travis-CI
-# in order to keep build small (in both duration and disk space)
-if [[ $TRAVIS ]]
-then
-  # Test suite package not relevant on Travis-CI
-  sed 's|DINSTALL_MYSQLTESTDIR=share/mysql/mysql-test|DINSTALL_MYSQLTESTDIR=false|' -i debian/rules
-  sed '/Package: mariadb-test-data/,/^$/d' -i debian/control
-  sed '/Package: mariadb-test$/,/^$/d' -i debian/control
-
-  # Extra plugins such as Mroonga, Spider, OQgraph, Sphinx and the embedded build can safely be skipped
-  sed 's|-DDEB|-DPLUGIN_MROONGA=NO -DPLUGIN_ROCKSDB=NO -DPLUGIN_SPIDER=NO -DPLUGIN_OQGRAPH=NO -DPLUGIN_PERFSCHEMA=NO -DPLUGIN_SPHINX=NO -DWITH_EMBEDDED_SERVER=OFF -DDEB|' -i debian/rules
-  sed "/Package: mariadb-plugin-mroonga/,/^$/d" -i debian/control
-  sed "/Package: mariadb-plugin-rocksdb/,/^$/d" -i debian/control
-  sed "/Package: mariadb-plugin-spider/,/^$/d" -i debian/control
-  sed "/Package: mariadb-plugin-oqgraph/,/^$/d" -i debian/control
-  sed "/ha_sphinx.so/d" -i debian/mariadb-server-10.6.install
-  sed "/Package: libmariadbd19/,/^$/d" -i debian/control
-  sed "/Package: libmariadbd-dev/,/^$/d" -i debian/control
-fi
-
-if [[ $(arch) =~ i[346]86 ]]
-then
-  sed "/Package: mariadb-plugin-rocksdb/,/^$/d" -i debian/control
-fi
-
-# If rocksdb-tools is not available (before Debian Buster and Ubuntu Disco)
-# remove the dependency from the RocksDB plugin so it can install properly
-# and instead ship the one built from MariaDB sources
-if ! apt-cache madison rocksdb-tools | grep 'rocksdb-tools' >/dev/null 2>&1
-then
-  sed '/rocksdb-tools/d' -i debian/control
-  sed '/sst_dump/d' -i debian/not-installed
-  echo "usr/bin/sst_dump" >> debian/mariadb-plugin-rocksdb.install
-fi
-
 # From Debian Buster/Ubuntu Bionic, libcurl4 replaces libcurl3.
 if ! apt-cache madison libcurl4 | grep 'libcurl4' >/dev/null 2>&1
 then
@@ -93,13 +38,6 @@ dch -b -D "${CODENAME}" -v "${EPOCH}${UPSTREAM}${PATCHLEVEL}~${CODENAME}" "Autom
 
 echo "Creating package version ${EPOCH}${UPSTREAM}${PATCHLEVEL}~${CODENAME} ... "
 
-# On Travis CI and Gitlab-CI, use -b to build binary only packages as there is
-# no need to waste time on generating the source package.
-if [[ $TRAVIS ]]
-then
-  BUILDPACKAGE_FLAGS="-b"
-fi
-
 # Use eatmydata is available to build faster with less I/O, skipping fsync()
 # during the entire build process (safe because a build can always be restarted)
 if which eatmydata > /dev/null
@@ -110,7 +48,7 @@ fi
 # Build the package
 # Pass -I so that .git and other unnecessary temporary and source control files
 # will be ignored by dpkg-source when creating the tar.gz source package.
-fakeroot $BUILDPACKAGE_PREPEND dpkg-buildpackage -us -uc -I $BUILDPACKAGE_FLAGS
+fakeroot $BUILDPACKAGE_PREPEND dpkg-buildpackage -us -uc -I $BUILDPACKAGE_FLAGS -b
 
 # If the step above fails due to missing dependencies, you can manually run
 #   sudo mk-build-deps debian/control -r -i
